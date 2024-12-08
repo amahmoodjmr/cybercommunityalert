e8bca547905fbadb0b2870972b084c604b3062a6
amahmoodjmr <152875408+amahmoodjmr@users.noreply.github.com>
2024-12-08 10:37:42 +0100

    Delete template directory

--- template/_init_.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-db = SQLAlchemy(app)
-
-from community_cyber_alert import models  # Import your models here
